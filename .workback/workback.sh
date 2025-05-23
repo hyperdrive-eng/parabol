99904e6889f68c594676056e176ba0cddac1d19b
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2025-05-23 21:21:17 +0100

    chore(.workback): adds workback.sh to pipe docker logs into file

--- .workback/workback.sh
+++ .workback/workback.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-nvm use 22.14
-cp .env.example .env
-pnpm i
+cp .env.example .env && \
+pnpm i && \
 # Start docker manually - docker daemon should be running
-pnpm db:start
-pnpm relay:build
+pnpm db:start && \
+pnpm relay:build && \
 pnpm dev 2>&1 | tee .workback/dev_environment.log
