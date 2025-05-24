295bb244988b18152c5f797d6f07a9ee587c09a1
Priyank Chodisetti <priyank.ch@gmail.com>
2025-05-24 01:31:04 -0700

    fix(date): type check date before calling toJSON()

--- packages/server/postgres/queries/updateJiraSearchQueries.ts
+++ packages/server/postgres/queries/updateJiraSearchQueries.ts
@@ -19,11 +19,21 @@ const updateJiraSearchQueries = async (params: UpdateJiraSearchQueryParams) => {
   const updateParams = {
     ...params,
     jiraSearchQueries: params.jiraSearchQueries.map((jsq) => {
+      let lastUsedAtDate: Date
+
+      if (jsq.lastUsedAt instanceof Date) {
+        lastUsedAtDate = jsq.lastUsedAt
+      } else if (jsq.lastUsedAt) {
+        lastUsedAtDate = new Date(jsq.lastUsedAt)
+      } else {
+        lastUsedAtDate = new Date()
+      }
+
       return {
         id: jsq.id,
         queryString: jsq.queryString,
         projectKeyFilters: jsq.projectKeyFilters,
-        lastUsedAt: jsq.lastUsedAt.toJSON(),
+        lastUsedAt: lastUsedAtDate.toJSON(),
         isJQL: jsq.isJQL
       }
     })
